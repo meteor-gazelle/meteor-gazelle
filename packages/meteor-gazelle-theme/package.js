b43a6ab4c436b54f1cb5873c82a2f22ac8b17e33
ajax-wcd <ajax@what.cd>
2015-10-27 21:13:30 -0700

    Bourbon and friends are functioning

--- packages/meteor-gazelle-theme/package.js
+++ packages/meteor-gazelle-theme/package.js
@@ -7,14 +7,19 @@ Package.describe({
 });
 
 Package.onUse(function (api) {
+
   api.versionsFrom('1.2.0.2');
 
   api.use([
-    'fourseven:scss@3.4.0-beta1',
+    'fourseven:scss@3.4.1',
     'wolves:bourbon@3.1.0',
     'wolves:neat@3.1.0',
     'fortawesome:fontawesome@4.4.0'
   ]);
+
+  api.addFiles([
+    'lib/style.scss'
+  ], 'client');
 });
 
 Package.onTest(function (api) {
